190ade85b58c998650ae100265df887d6cd93782
philippts97 <81205980+philippts97@users.noreply.github.com>
2021-04-02 20:25:21 +1000

    Update app3.js

--- app3.js
+++ app3.js
@@ -44,18 +44,18 @@ let srok;
   
     const page = await browser.newPage(); // Новая страница
 
-    const previousSession = fs.existsSync(cookiesFilePath);
-    if (previousSession) {
-      // If file exist load the cookies
-      const cookiesString = fs.readFileSync(cookiesFilePath);
-      const parsedCookies = JSON.parse(cookiesString);
-      if (parsedCookies.length !== 0) {
-        for (let cookie of parsedCookies) {
-          await page.setCookie(cookie)
-        }
-        console.log('Session has been loaded in the browser')
-      }
-    }
+ //   const previousSession = fs.existsSync(cookiesFilePath);
+ //   if (previousSession) {
+ //     // If file exist load the cookies
+ //     const cookiesString = fs.readFileSync(cookiesFilePath);
+ //     const parsedCookies = JSON.parse(cookiesString);
+ //     if (parsedCookies.length !== 0) {
+ //       for (let cookie of parsedCookies) {
+ //         await page.setCookie(cookie)
+ //       }
+ //       console.log('Session has been loaded in the browser')
+ //     }
+ //   }
     
     await page.goto('https://private.proverki.gov.ru/', {waitUntil: 'networkidle2', timeout: 0}); // Переход на сайт ЕРП
     
